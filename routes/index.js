b66cfa4f70004cbff7d77d9ff53ebd32f3c3ea9e
Jiao Jing Ping <elleryjiao@gmail.com>
2013-11-21 13:30:33 +0800

    before merge

--- routes/index.js
+++ routes/index.js
@@ -2,22 +2,15 @@
  * Base level routes
  */
 module.exports = function (app,passport) {
-	require('./user')(app);
+	require('./user')(app,passport);
+	
+	// index page
 	app.get("/", function (req, res) {
-		var body = 'Hello World';
-        res.setHeader('Content-Type', 'text/plain');
+		var body = '<a href="auth/facebook">login with facebook</a>';
+        res.setHeader('Content-Type', 'text/html');
         res.setHeader('Content-Length', body.length);
         res.end(body);
 	});
-	app.get("/auth/facebook", passport.authenticate("facebook",{ scope : "email"}));
-	app.get("/auth/facebook/callback",
-		passport.authenticate("facebook",{ failureRedirect: '/login'}),
-		function(req,res){
-			var body = req.user.name;
-			res.setHeader('Content-Type', 'text/plain');
-			res.setHeader('Content-Length', body.length);
-			res.end(body);
-		}
-	);
+	
 }
  
