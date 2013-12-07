70227ad3e33305507c97f08ad0d8e26a69a68978
Jiao Jingping <elleryjiao@gmail.com>
2013-12-08 04:01:08 +0800

    qq auth bug fixed

---
+++ app/models/auth.js
@@ -0,0 +1,20 @@
+var mongoose = require('mongoose')
+/*Model for third party authentication*/
+AuthSchema = mongoose.Schema({
+  _id:{type:String},
+	provider_id:{type:String,required:true,index:true},
+  provider:{type:String,required:true,index:true},
+	accessToken:String,
+	refreshToken:String,
+	profile:mongoose.Schema.Types.Mixed,
+	createdAt:{type:Date,default:Date.now},
+  updatedAt:{type:Date}
+});
+
+AuthSchema.pre('save', function(next){
+  this.updated_at = new Date;
+  next();
+})
+
+
+module.exports = AuthSchema;
