884284e5c423e18a4fc8bf5f1d25c6fe64b15250
Jiao Jingping <elleryjiao@gmail.com>
2013-12-08 23:02:02 +0800

    added security token

--- app/models/auth.js
+++ app/models/auth.js
@@ -1,4 +1,6 @@
 var mongoose = require('mongoose')
+var HASH_SECRET = "wowcheer-rocks";
+var md5 = require('MD5');
 /*Model for third party authentication*/
 AuthSchema = mongoose.Schema({
   user:{type:String},
@@ -16,5 +18,26 @@ AuthSchema.pre('save', function(next){
   next();
 })
 
+/*Verify cash*/
+AuthSchema.statics.varifyHash = function(provider,provider_id,hash){
+  return md5(provider + HASH_SECRET + provider_id) === hash;
+}
+/*Generate hash*/
+AuthSchema.statics.hash = function(){
+  return md5(this.provider+HASH_SECRET+this.provider_id); 
+}
+
+/*Verify auth provider*/
+AuthSchema.statisc.varifyProvider = function(provider,provider_id,hash,callback) {
+  var q = this.findOne({provider:provider,provider_id:provider_id});
+  q.exec(function(err,providerUser) {
+    if (err) throw err;
+    if (providerUser) {
+      callback(this.varifyHash(providerUser.provider,providerUser.provider_id,hash),providerUser);
+    } else {
+      callback(false,null);
+    }
+  })
+};
 
 module.exports = AuthSchema;
